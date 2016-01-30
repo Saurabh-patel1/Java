b4629a68a017f8fb6d79cdc67933fd11451c6b36
Saurabh-patel1 <saurabpatel12@yahoo.com>
2016-01-29 20:21:11 -0600

    Assignment2

--- Class 2/Month.java
+++ Class 2/Month.java
@@ -1,19 +1,29 @@
 class Month
 {
-public static void main(String[] args)
+public static void main(String args[])
 {
 	System.out.println(args[0]);
-int month=args[0];
+//int month=args[0];
 	
 //char month=char.parseChar(args[0]);
-
+String month=args[0];
 int b=31,c=30,d=28;
 switch(month)
 {
-case 'J':
+case "January":
 System.out.println(b);
 break;
-//case "Febuary":System.out.println(d);break;
-default:System.out.println("Not a valid month");
+case "Febuary":System.out.println(d);break;
+case "March":System.out.println(b);break;
+case "April":System.out.println(c);break;
+case "May":System.out.println(b);break;
+case "June":System.out.println(c);break;
+case "July":System.out.println(b);break;
+case "August":System.out.println(b);break;
+case "September":System.out.println(c);break;
+case "October":System.out.println(b);break;
+case "November":System.out.println(c);break;
+case "December":System.out.println(b);break;
+default:System.out.println("Not a valid month");break;
 
 }}}
